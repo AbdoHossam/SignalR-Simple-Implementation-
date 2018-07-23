00f6c858e95ab88125798a46e74ac86bd62ff39f
Abdelrahman Hossam Ahmed <AbdelrahmanRyad70@Gmail.com>
2018-07-23 12:01:33 +0200

    SignalR-Tutorial Implemention
    Implement SignalR- Tutorial based on videos maked by Caleb Curry :)

---
+++ CTVM2App/Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("It Worked!");
+            $.connection.myHub.server.announce("Connected!");
+        })
+        .fail(function () { alert("ERROR!!!"); });
+             $.connection.myHub.client.announce = function (message) {
+        $("#weclome-messages").append(message + "<br />");
+        }
+})() 
