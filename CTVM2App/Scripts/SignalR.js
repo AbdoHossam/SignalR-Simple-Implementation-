de9c71ba216ae8360474e57e0e45ad8a6bae2617
Abdelrahman Hossam Ahmed <AbdelrahmanRyad70@Gmail.com>
2018-07-23 12:14:39 +0200

    Create a javaScript function
    Create anew function To Write Direct into the page

--- CTVM2App/Scripts/SignalR.js
+++ CTVM2App/Scripts/SignalR.js
@@ -5,7 +5,10 @@
             $.connection.myHub.server.announce("Connected!");
         })
         .fail(function () { alert("ERROR!!!"); });
-             $.connection.myHub.client.announce = function (message) {
+    $.connection.myHub.client.announce = function (message) {
+        writeToPage();
+    }
+    var writeToPage = function () {
         $("#weclome-messages").append(message + "<br />");
-        }
+    }
 })() 
