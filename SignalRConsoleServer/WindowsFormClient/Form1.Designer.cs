4a9a7a53b3473c84a4980556a994b21804d08e71
Abdelrahman Hossam Ahmed <AbdelrahmanRyad70@Gmail.com>
2018-07-26 17:09:15 +0200

    Web Client
    Create A web client for Console Server :100:

---
+++ SignalRConsoleServer/WindowsFormClient/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbMessage = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.RtbShow = new System.Windows.Forms.RichTextBox();
+            this.TbName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TbMessage
+            // 
+            this.TbMessage.Location = new System.Drawing.Point(13, 15);
+            this.TbMessage.Multiline = true;
+            this.TbMessage.Name = "TbMessage";
+            this.TbMessage.Size = new System.Drawing.Size(346, 21);
+            this.TbMessage.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(265, 45);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(32, 45);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(143, 45);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // RtbShow
+            // 
+            this.RtbShow.Location = new System.Drawing.Point(13, 74);
+            this.RtbShow.Name = "RtbShow";
+            this.RtbShow.ReadOnly = true;
+            this.RtbShow.Size = new System.Drawing.Size(346, 246);
+            this.RtbShow.TabIndex = 5;
+            this.RtbShow.Text = "";
+            // 
+            // TbName
+            // 
+            this.TbName.Location = new System.Drawing.Point(12, 15);
+            this.TbName.Name = "TbName";
+            this.TbName.Size = new System.Drawing.Size(206, 20);
+            this.TbName.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 332);
+            this.Controls.Add(this.TbName);
+            this.Controls.Add(this.RtbShow);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.TbMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbMessage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox RtbShow;
+        private System.Windows.Forms.TextBox TbName;
+    }
+}
+
---
+++ SignalRConsoleServer/WindowsFormClient/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormClient
+{
+    public partial class Form1 : Form
+    {
+        IHubProxy _hub;
+        string url = @"http://localhost:8080/";
+        
+        public Form1()
+        {
+            InitializeComponent();
+            TbMessage.Visible = false;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            TbMessage.Text = String.Empty;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (TbName.Text != null && TbName.Text !=" ")
+            {
+                var connection = new HubConnection(url);
+                _hub = connection.CreateHubProxy("MyHub");
+                connection.Start().Wait();
+                TbName.Visible = false;
+                TbMessage.Visible= true;
+                btnLogin.Visible = false;
+                btnLogin.Enabled = false;
+            }
+            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            
+                _hub.Invoke("Send", TbName.Text, TbMessage.Text);
+            _hub.On<string, string>("addMessage", (_name, _message) =>
+                    Invoke((Action)(() =>
+                   RtbShow.Text += _name + " "+ _message + "\r")));
+
+            TbMessage.Text = string.Empty;
+            RtbShow.Text = string.Empty;
+            TbMessage.Focus();
+            _hub.Subscribe("addMessage");
+            
+        }
+    }
+}
